8cdc17d556f5fb318edcd3bf287d0dd0b55f19e6
yurtsevichK <nicecurls@icloud.com>
2019-11-07 21:23:13 +0300

    commit jd01_04 TaskA

--- src/by/it/yurtsevich/jd01_04/TaskC.java
+++ src/by/it/yurtsevich/jd01_04/TaskC.java
@@ -2,16 +2,7 @@ package by.it.yurtsevich.jd01_04;
 
 import java.util.Scanner;
 
-public class TaskA {
-    private static void printMulTable() {
-        for (int i = 2; i <= 9; i++) {
-            for (int j = 2; j <= 9; j++) {
-                System.out.printf("%1d*%1d=%-2d ", i, j, i * j);
-            }
-            System.out.println();
-        }
-    }
-
+public class TaskC {
     private static void buildOneDimArray(String line) {
         double[] array = InOut.getArray(line);
         double start = array[0];
@@ -33,9 +24,10 @@ public class TaskA {
         }
     }
     public static void main(String[] args) {
-        printMulTable();
         Scanner sc = new Scanner(System.in);
         String s = sc.nextLine();
         buildOneDimArray(s);
     }
+
+
     }
