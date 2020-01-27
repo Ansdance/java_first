8417dbbf9fce2c36cba12187d45b6998e8f4d164
yurtsevichK <nicecurls@icloud.com>
2020-01-27 12:36:29 +0300

    videocourse lesson1 + homework1

--- src/by/it/yurtsevich/videoCourse/lesson1/Lesson1.java
+++
@@ -1,7 +0,0 @@
-package by.it.yurtsevich.videoCourse.lesson1;
-
-public class Lesson1 {
-    public static void main(String[] args) {
-        System.out.println("Welcome to the first lessons");
-    }
-}
---
+++ src/by/it/yurtsevich/videoCourse/lesson1/Test1.java
@@ -0,0 +1,20 @@
+package by.it.yurtsevich.videoCourse.lesson1;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the first lesson!");
+        System.out.println("Hello...");
+        System.out.println("Меня зовут ");
+        System.out.println("Костя");
+        System.out.print("A ");
+        System.out.println("a");
+        System.out.print("B ");
+        System.out.println("b");
+        System.out.print("C ");
+        System.out.println("c");
+        System.out.println("abc");
+        System.out.println();
+        System.out.println("def");
+    }
+}
+
