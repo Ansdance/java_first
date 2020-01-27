bf256bed3d66c3a7306d6fc1bcc7fb169d7d2349
yurtsevichK <nicecurls@icloud.com>
2020-01-27 13:46:50 +0300

    videocourse lesson2

---
+++ src/by/it/yurtsevich/videoCourse/lesson2/PrimitiveDataTypes.java
@@ -0,0 +1,34 @@
+package by.it.yurtsevich.videoCourse.lesson2;
+
+public class PrimitiveDataTypes {
+    public static void main(String[] args) {
+        byte b1 = 5;
+        byte b2 = 10;
+        byte b3 = 15;
+
+        short s1 = 20;
+        short s2 = 30;
+        short s3 = -40;
+
+        int i1 = 1231;
+        int i2 = -1231;
+        int i3 = -133231;
+
+        long l1 = 10000L; // L в конце
+
+        float f1 = 212424.2424F; // F в конце
+
+        double d1 = 2424.2342; // или с D в конце
+        double d2 = 24214.2133D;
+
+        char c1 = 's';
+        char c2 = 'S';
+        char c3 = 499; // отражает 499ый символ в юникоде ǳ - ЗДЕСЬ 10 система счисления
+        char c4 = '\u0499'; // ҙ - здесь 16 система счисления
+
+        System.out.println(c2);
+        System.out.println(d1);
+        System.out.println(c3);
+        System.out.println(c4);
+    }
+}
