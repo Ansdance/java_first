8285f0df583d5ebf89f9a22287bf52610e9daa32
yurtsevichK <nicecurls@icloud.com>
2020-01-29 01:41:06 +0300

    homework 5

--- src/by/it/yurtsevich/videoCourse/lesson4/BankAccount.java
+++ src/by/it/yurtsevich/videoCourse/lesson4/BankAccount.java
@@ -1,11 +1,33 @@
 package by.it.yurtsevich.videoCourse.lesson4;
 
 public class BankAccount {
+    double balance1;
+    double popoplnenieScheta(double dengi) {
+        balance1+=dengi;
+        return balance;
+    }
+
+    double snyatieSoScheta(double dengi) {
+        balance1-=dengi;
+        return balance;
+    }
+    void showinfo(){
+        System.out.println(balance1);
+    }
+
     int id;
     String name;
     double balance;
 
     public static void main(String[] args) {
+        BankAccount bA = new BankAccount();
+        bA.balance1+=10;
+        bA.showinfo();
+        bA.balance1+=100.20;
+        bA.showinfo();
+        bA.balance1-=50;
+        bA.showinfo();
+
         BankAccount MyAccount = new BankAccount();
         MyAccount.id=1;
         MyAccount.name="Kostya";
