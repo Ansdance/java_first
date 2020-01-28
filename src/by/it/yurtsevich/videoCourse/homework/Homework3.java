9b62d275a8564fdddf6d6e2caaba04b14b473032
yurtsevichK <nicecurls@icloud.com>
2020-01-28 13:52:32 +0300

    homework3

--- src/by/it/yurtsevich/videoCourse/homework/Homework3.java
+++ src/by/it/yurtsevich/videoCourse/homework/Homework3.java
@@ -2,6 +2,12 @@ package by.it.yurtsevich.videoCourse.homework;
 
 public class Homework3 {
     public static void main(String[] args) {
+        int i1 = 5, i2 = 11;
+        double d1 = 55, d2 = 1.3;
+        long l = 20L;
+        double result = i2 / d1 + d2 % i1 - l;
+        System.out.println(result);
+
 
     }
 }
