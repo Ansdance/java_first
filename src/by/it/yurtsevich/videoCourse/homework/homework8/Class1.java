443301e07a991f4263e0d55a133a76f81f85f675
yurtsevichK <nicecurls@icloud.com>
2020-02-01 23:54:40 +0300

    homewrok 8

---
+++ src/by/it/yurtsevich/videoCourse/homework/homework8/Class1.java
@@ -0,0 +1,32 @@
+package by.it.yurtsevich.videoCourse.homework.homework8;
+
+public class Class1 {
+    public static double triChisla() {
+        double d1 = 3.33;
+        double d2 = 3213.33;
+        double d3 = 343.33;
+        double result = d1 * d2 * d3;
+        return result;
+    }
+
+    public static void dvaChisla() {
+        double d4 = 213.32;
+        double d5 = 113.32;
+        double result1 = d4 / d5;
+    }
+
+    final static double p = 3.14;
+
+    public double ploshad() {
+        double radius = 34.2;
+        double ploshad1 = p * radius * radius;
+        return ploshad1;
+    }
+    public static double dlinaOkr(){
+        double radius =56.3;
+        double ploshad2=2* p*radius;
+        return ploshad2;
+    }
+
+}
+
---
+++ src/by/it/yurtsevich/videoCourse/homework/homework8/Class2.java
@@ -0,0 +1,10 @@
+package by.it.yurtsevich.videoCourse.homework.homework8;
+
+public class Class2 {
+    public static void main(String[] args) {
+        Class1 c1 = new Class1();
+        System.out.println(Class1.triChisla());
+        System.out.println(Class1.dlinaOkr());
+        //System.out.println(Class1.dvaChisla();
+    }
+}
